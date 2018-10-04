df6ce59a7d2a059bcd5dfe6f58f06f8fc3a1a87e
batuhaandogan <tipala_1909@hotmail.com>
2018-10-04 15:20:14 -0400

    works now

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ class App extends Component {
     if( this.state.loggedInUser === null ){
       this.service.loggedin()
       .then(response =>{
-        // this.props.history.push('/lobby');
+
           this.setState({
-            // redirect: true,
+
             loggedInUser:  response
         }) 
       })
@@ -49,42 +49,24 @@ class App extends Component {
     }
   }
 
-//  lobbyIsIn()  {
-//    if(this.state.loggedInUser === true) {
-//      <Navbar />
 
-//    }
-//  }
 
 
   render() {
     console.log(this.state)
     this.fetchUser();
-    // if(this.state.loggedInUser !== null){
-    //   this.setState({redirect: true})
-    // } else {
-    //   this.setState({redirect: false})
-    // }
-    // if(this.state.redirect) {
-    //   return <Redirect to="/lobby" />
-    // } else {
 
-    // (this.state.loggedInUser ? <Redirect to="/lobby" /> : <Redirect to="/" />) 
 
       return (  
         <div>
   
           <div className="move-it-all App another-class">
           <Navbar {...this.props} setTheUserInTheAppComponent={this.logMeIn} userInSession={this.state.loggedInUser} />
-            {/* <Switch>
-              <Route className="for-both" exact path="/" render={() => <Login setTheUserInTheAppComponent={this.logMeIn}/>}/>
-            </Switch> */}
-          </div>
-  {/* <li>
-  <Link to='/lobby'>Lobby</Link>
-</li> */}
-        <Switch>
           <Route className="for-both" exact path="/" render={() => <Login {...this.props} setTheUserInTheAppComponent={this.logMeIn}/> }/>
+          </div>
+      
+
+        <Switch>
           <Route exact path="/lobby" component={Lobby} />
           <Route exact path="/signup" component={Signup} />
         <Signup/>
